97efec1d58128996530b7d0aacda11e5f64ce1f3
Davit Manasyan <davit.manasyan.07@gmail.com>
2025-10-01 11:50:29 +0400

    little changes

---
+++ recipe-front/src/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <h2>Recipe App</h2>
+            <ul>
+                <li>Home</li>
+                <li>Recipes</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    );
+}
