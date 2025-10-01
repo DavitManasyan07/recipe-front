d8f1e4757ecb38bdc40aa1a6a3a3bb474899ea62
Sogomonyan Hayk <sogomonyanhayk7@gmail.com>
2025-10-01 12:32:55 +0400

    installed router package, simple setup, simple navbar added

--- recipe-front/src/App.js
+++ recipe-front/src/App.js
@@ -1,27 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
+import Main from "./Main/Main"
+import { BrowserRouter, Routes, Route } from "react-router";
+
 
 function App() {
     return (
-        <div className="App">
-            <NavBar></NavBar>
-            
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <NavBar></NavBar>
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route></Route> {/*stx de matainim routenen setup kanis */}
+                    <Route></Route>
+                    <Route></Route>
+                    <Route></Route>
+                    <Route></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- recipe-front/src/NavBar/NavBar.js
+++ recipe-front/src/NavBar/NavBar.js
@@ -1,12 +1,36 @@
 export default function NavBar() {
     return (
-        <nav>
-            <h2>Recipe App</h2>
-            <ul>
-                <li>Home</li>
-                <li>Recipes</li>
-                <li>About</li>
-            </ul>
+        <nav className="navbar"> {/* logged in*/}
+            <div className="navbar--part left">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/add-recipe">Add recipe</a></li>
+                    <li><a href="/categories">Categories</a></li>
+                    <li><a href="/favorites">Favorites</a></li>
+                    <li><a href="/my-recipes">My recipes</a></li>
+                </ul>
+            </div>
+            <div className="navbar--part right">
+                <ul>
+                    {/* <li><a href="/profile">Profile</a></li> stex profili nkar pen kare ini bayc xuyevo profile ini vobshe ta che*/}
+                    <li><a href="/logout">Logout</a></li>
+                </ul>
+            </div>
         </nav>
+
+        // <nav className="navbar"> {/* not logged in*/}
+        //     <div className="navbar--part left">
+        //         <ul>
+        //             <li><a href="/">Home</a></li>
+        //             <li><a href="/categories">Categories</a></li>
+        //         </ul>
+        //     </div>
+        //     <div className="navbar--part right">
+        //         <ul>
+        //             <li><a href="/login">Login</a></li>
+        //             <li><a href="/signup">Sign up</a></li>
+        //         </ul>
+        //     </div>
+        // </nav>
     );
 }
