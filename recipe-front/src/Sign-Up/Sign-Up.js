d4f38b32886558a335ca9a38928bc90c9d3f32fe
Sogomonyan Hayk <sogomonyanhayk7@gmail.com>
2025-10-05 18:26:12 +0400

    patar css um horhor yral lii braaaaaat

--- recipe-front/src/Sign-Up/Sign-Up.js
+++ recipe-front/src/Sign-Up/Sign-Up.js
@@ -4,26 +4,37 @@ import "./Sign-Up.css";
 export default function SignUp() {
   return (
     <div className="signUpBody">
-      <header>Sign Up</header>
-      <form action="singUp">
+      <div className="signUpHeader">
+        <h1>Sign Up</h1>
+        <p>Create your account and join us!</p>
+      </div>
+
+      <form action="signUp">
         <div className="field">
-          <input type="text" required placeholder="Username" />
+          <input type="text" required />
+          <label>Full Name</label>
         </div>
         <div className="field">
-          <input type="email" required placeholder="Email" />
+          <input type="text" required />
+          <label>Username</label>
         </div>
         <div className="field">
-          <input
-            type="password"
-            className="pass-key"
-            required
-            placeholder="Password"
-          />
+          <input type="email" required />
+          <label>Email</label>
+        </div>
+        <div className="field">
+          <input type="password" className="pass-key" required />
+          <label>Password</label>
+        </div>
+        <div className="field">
+          <input type="password" className="pass-key" required />
+          <label>Confirm Password</label>
         </div>
         <div className="field-button">
-          <Button link={""} text={"Sign Up"}/>
+          <Button link={""} text={"Sign Up"} />
         </div>
       </form>
+
       <div className="link">
         Already a member? <a href="signIn">Login here</a>
       </div>
